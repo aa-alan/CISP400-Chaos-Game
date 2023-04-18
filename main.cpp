734b523503bfc2cfc83fcdc6e8e785629cafc13e
Jeffrey Lenh <130512182+jlenh32@users.noreply.github.com>
2023-04-18 16:14:56 -0700

    Allowed for input within app, up to octagon

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
 {
 	// Seeding rand()
 	srand(time(0));
-	const int MAX_VERTICES = 3; // For rand(); for vector indexes 0-2
+	//const int MAX_VERTICES = 4; // For rand(); for vector indexes 0-2
 
 	// Create a video mode object
 	VideoMode vm(1920, 1080);
@@ -20,13 +20,19 @@ int main()
 	// For handling mouse-related events
 	sf::Event event;
 
+	//For handling keyboard-related events
+	sf::Keyboard input;
+
+	//Stores number of vertices
+	int inputVertices = 0;
+
 	// Loading font to disk, exit program on failure to load font
 	Font titleFont;
 	if (!titleFont.loadFromFile("fonts/Roboto-BoldItalic.ttf"))
 	{
 		window.close();
 	}
-	
+
 	// Declaring text object
 	Text title; // Chaos Game title
 	Text instruct; // lets user know what to do
@@ -51,43 +57,100 @@ int main()
 		window.close();
 	}
 	instruct.setFont(instructFont);
-	instruct.setString("Click three times on the screen to set rectangle vertices...");
+	instruct.setString("Enter how many vertices to construct.");
 	instruct.setCharacterSize(60);
 
 	// To center instructions at bottom of screen
 	textRect = instruct.getLocalBounds();
 	instruct.setOrigin(textRect.left + textRect.width / 2.0, 0);
 	instruct.setPosition(window.getSize().x / 2.0, window.getSize().y / 1.2);
-	
+
 	// Setting up mouse click tracking
 	int mouseClick = 0;
 	vector<float> mousePosX;
 	vector<float> mousePosY;
+
+	//Setting up keyboard input
+	
 
 	// Chaos Game algorithm starts when 5 clicks have been registered
 	bool algorithmStart = false;
 
 	while (window.isOpen())
 	{
-		while (window.pollEvent(event) && mouseClick < 5)
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num3))
+		{
+			instruct.setString("Triangle selected.");
+			inputVertices = 2; //Starting at 0
+			textRect = instruct.getLocalBounds();
+			instruct.setOrigin(textRect.left + textRect.width / 2.0, 0);
+			instruct.setPosition(window.getSize().x / 2.0, window.getSize().y / 1.2);
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num4))
+		{
+			instruct.setString("Rectangle selected.");
+			inputVertices = 3; //Starting at 0
+			textRect = instruct.getLocalBounds();
+			instruct.setOrigin(textRect.left + textRect.width / 2.0, 0);
+			instruct.setPosition(window.getSize().x / 2.0, window.getSize().y / 1.2);
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num5))
+		{
+			instruct.setString("Pentagon selected.");
+			inputVertices = 4; //Starting at 0
+			textRect = instruct.getLocalBounds();
+			instruct.setOrigin(textRect.left + textRect.width / 2.0, 0);
+			instruct.setPosition(window.getSize().x / 2.0, window.getSize().y / 1.2);
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num6))
+		{
+			instruct.setString("Hexagon selected.");
+			textRect = instruct.getLocalBounds();
+			inputVertices = 5; //Starting at 0
+			instruct.setOrigin(textRect.left + textRect.width / 2.0, 0);
+			instruct.setPosition(window.getSize().x / 2.0, window.getSize().y / 1.2);
+		}
+		
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num7))
+		{
+			instruct.setString("Sextagon selected.");
+			inputVertices = 6; //Starting at 0
+			textRect = instruct.getLocalBounds();
+			instruct.setOrigin(textRect.left + textRect.width / 2.0, 0);
+			instruct.setPosition(window.getSize().x / 2.0, window.getSize().y / 1.2);
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num8))
+		{
+			instruct.setString("Octagon selected.");
+			textRect = instruct.getLocalBounds();
+			inputVertices = 7; //Starting at 0
+			instruct.setOrigin(textRect.left + textRect.width / 2.0, 0);
+			instruct.setPosition(window.getSize().x / 2.0, window.getSize().y / 1.2);
+		}
+		
+		while (window.pollEvent(event) && mouseClick < inputVertices + 2)
 		{
 			if (event.type == sf::Event::MouseButtonPressed)
 			{
-				if (event.mouseButton.button == sf::Mouse::Left 
+				if (event.mouseButton.button == sf::Mouse::Left
 					|| event.mouseButton.button == sf::Mouse::Right)
 				{
 					mouseClick++;
 					mousePosX.push_back(event.mouseButton.x);
 					mousePosY.push_back(event.mouseButton.y);
 					// Mouse click text decisions
-					if (mouseClick == 4)
+					if (mouseClick == inputVertices + 1)
 					{
 						instruct.setString("Click once more to start the algorithm!");
 						textRect = instruct.getLocalBounds();
 						instruct.setOrigin(textRect.left + textRect.width / 2.0, 0);
 						instruct.setPosition(window.getSize().x / 2.0, window.getSize().y / 1.2);
 					}
-					if (mouseClick > 4)
+					if (mouseClick > inputVertices + 1)
 					{
 						// Remove text from screen
 						title.setFillColor(sf::Color::Black);
@@ -115,7 +178,15 @@ int main()
 			int drawSpeed = mousePosX.size() / 25 + 1; // adjust speed by vector size
 			for (int i = 0; i < drawSpeed; i++)
 			{
-				int vertex = rand() % (MAX_VERTICES + 3);
+				int vertex;
+				if (inputVertices == 2)
+				{
+					vertex = rand() % (inputVertices + 1);
+				}
+				else
+				{
+					vertex = rand() % (inputVertices + 1);
+				}
 				float midpointX = (mousePosX[vertex] + mousePosX.back()) / 2.0;
 				float midpointY = (mousePosY[vertex] + mousePosY.back()) / 2.0;
 
@@ -142,10 +213,11 @@ int main()
 		for (int i = 0; i < mousePosX.size(); i++)
 		{
 			point.setPosition(mousePosX[i], mousePosY[i]);
+			Color color(50, 125, mousePosY[i] / 100);
+			point.setFillColor(color);
 			window.draw(point);
 		}
 		window.display();
 	}
-
 	return 0;
 }
