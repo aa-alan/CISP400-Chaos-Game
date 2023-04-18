4e706e68f74bea0c21edd4d3e5bc21d100f11917
akushnir15@gmail.com <akushnir15@gmail.com>
2023-04-17 22:45:18 -0700

    Mouse events, vector storage for click coordinates

--- main.cpp
+++ main.cpp
@@ -12,6 +12,9 @@ int main()
 	// Create and open a window for the game
 	RenderWindow window(vm, "Chaos Game", Style::Default);
 
+	// For handling mouse-related events
+	sf::Event event;
+
 	// Loading font to disk, exit program on failure to load font
 	Font titleFont;
 	if (!titleFont.loadFromFile("fonts/Roboto-BoldItalic.ttf"))
@@ -22,6 +25,11 @@ int main()
 	// Declaring text object
 	Text title; // Chaos Game title
 	Text instruct; // lets user know what to do
+	Text mousePos;
+
+	mousePos.setFont(titleFont);
+	mousePos.setFillColor(sf::Color::White);
+	mousePos.setCharacterSize(24);
 
 	// Title processing
 	title.setFont(titleFont);
@@ -51,8 +59,40 @@ int main()
 	instruct.setOrigin(textRect.left + textRect.width / 2.0, 0);
 	instruct.setPosition(window.getSize().x / 2.0, window.getSize().y / 1.2);
 	
+	// Setting up mouse click tracking
+	int mouseClick = 0;
+	vector<int> mousePosX;
+	vector<int> mousePosY;
 	while (window.isOpen())
 	{
+		while (window.pollEvent(event) && mouseClick != 4)
+		{
+			if (event.type == sf::Event::MouseButtonPressed)
+			{
+				if (event.mouseButton.button == sf::Mouse::Left 
+					|| event.mouseButton.button == sf::Mouse::Right)
+				{
+					mouseClick++;
+					mousePosX.push_back(event.mouseButton.x);
+					mousePosY.push_back(event.mouseButton.y);
+					// Mouse click text decisions
+					if (mouseClick == 3)
+					{
+						instruct.setString("Click once more to ready the program");
+						textRect = instruct.getLocalBounds();
+						instruct.setOrigin(textRect.left + textRect.width / 2.0, 0);
+						instruct.setPosition(window.getSize().x / 2.0, window.getSize().y / 1.2);
+					}
+					if (mouseClick > 3)
+					{
+						// Remove text from screen
+						title.setFillColor(sf::Color::Black);
+						instruct.setFillColor(sf::Color::Black);
+					}
+				
+				}
+			}
+		}
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
 			window.close();
@@ -62,6 +102,12 @@ int main()
 		window.clear();
 		window.draw(title);
 		window.draw(instruct);
+
+		// Display points
+		for (int i = 0; i < mouseClick; i++)
+		{
+			
+		}
 		window.display();
 	}
 
